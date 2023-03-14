267117f3476833fd2dbc99df9c35b3c7a20345b7
MrZhouZh <17600330793@sina.cn>
2023-03-14 12:43:38 +0800

    feat: update import plugin

--- plugins/babel-plugin-import.js
+++ plugins/babel-plugin-import.js
@@ -4,7 +4,7 @@ const types = require('@babel/types');
 const visitor = {
   ImportDeclaration(path, state) {
     // 获取配置选项中的库的名称
-    const { libraryName, libraryDirectory = 'lib' } = state.opts
+    const { libraryName, libraryDirectory } = state.opts
 
     const { node } = path
     // 获取批量导入声明数组
@@ -14,7 +14,21 @@ const visitor = {
       node.source.value === libraryName &&
       !types.isImportDefaultSpecifier(specifiers[0])
     ) {
-      //
+      // 便来批量导入声明数组
+      const declarations = specifiers.map(specifier => {
+        return types.importDeclaration(
+          // 导入声明 importDeclaration flatten
+          [types.importDefaultSpecifier(specifier.local)],
+          // 导入模块 source lodash/flatten
+          types.stringLiteral(
+            libraryDirectory
+              ? `${libraryName}/${libraryDirectory}/${specifier.imported.name}`
+              : `${libraryName}/${specifier.imported.name}`
+          )
+        )
+      })
+      // 替换当前节点
+      path.replaceWithMultiple(declarations)
     }
   }
 }
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -8,25 +8,25 @@ module.exports = {
     path: path.resolve('dist'),
   },
   module: {
-    // rules: [
-    //   {
-    //     test: /\.js$/,
-    //     use: {
-    //       loader: 'babel-loader',
-    //       options: {
-    //         plugins: [
-    //           // 使用自己手写的 import 插件
-    //           [
-    //             path.resolve(__dirname, 'babel-plugin-import.js'),
-    //             {
-    //               libraryName: 'lodash',
-    //             }
-    //           ]
-    //         ]
-    //       }
-    //     }
-    //   }
-    // ]
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              // 使用自己手写的 import 插件
+              [
+                path.resolve(__dirname, 'plugins/babel-plugin-import.js'),
+                {
+                  libraryName: 'lodash',
+                }
+              ]
+            ]
+          }
+        }
+      }
+    ]
   }
 }
 
@@ -38,3 +38,6 @@ module.exports = {
 // import flatten from 'lodash/flatten'
 // import concat from 'lodash/concat'
 // asset bundle.js 26.9 KiB [emitted] (name: main)
+
+// --- 增加 babel-plugin-import 后 ---
+// asset bundle.js 26.8 KiB [emitted] (name: main)
