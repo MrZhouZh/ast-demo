054fe46a8ca6da21dce06ec90605b7cf36f15f67
MrZhouZh <17600330793@sina.cn>
2023-03-14 12:33:40 +0800

    feat: import plugin get specifiers

--- plugins/babel-plugin-import.js
+++ plugins/babel-plugin-import.js
@@ -5,6 +5,17 @@ const visitor = {
   ImportDeclaration(path, state) {
     // 获取配置选项中的库的名称
     const { libraryName, libraryDirectory = 'lib' } = state.opts
+
+    const { node } = path
+    // 获取批量导入声明数组
+    const { specifiers } = node
+    // 如果当前的节点模块名称是我们需要的库名称, 并且导入不是默认导入
+    if(
+      node.source.value === libraryName &&
+      !types.isImportDefaultSpecifier(specifiers[0])
+    ) {
+      //
+    }
   }
 }
 
