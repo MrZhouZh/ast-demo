bd134c41e7d0edb4cff97a42a47720b0c1ea5077
MrZhouZh <17600330793@sina.cn>
2023-04-09 17:10:12 +0800

    feat: tree shaking unnecessary

--- webpack.config.treeshaking.js
+++ webpack.config.treeshaking.js
@@ -1,19 +1,13 @@
 /**
  * tree-shaking 测试分析
- * refs: https://juejin.cn/post/7002410645316436004
- * 启动 tree shaking 必须满足三个条件
- * - 使用 ESM 规范
- * - optimization.usedExports 为 true, 启动标记功能
- * - 启动代码优化功能
- *  - 配置 mode = production
- *  - 配置 optimization.minimize = true
- *  - 提供 optimization.minimizer 数组
  */
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: './tree-shaking/index.js',
+  // mode: 'development',
+  mode: process.env.NODE_ENV,
+  // entry: './tree-shaking/index.js',
+  entry: './tree-shaking/unnecessary.js',
   devtool: false,
   module: {
     rules: [
@@ -28,10 +22,14 @@ module.exports = {
 
     // optimization.usedExports: true
     filename: '[name].js',
-    path: path.join(__dirname, './tree-shaking/dist')
+    // path: path.join(__dirname, './tree-shaking/dist'),
+    // unnecessary assign value
+    path: path.join(__dirname, './tree-shaking/unnecessary-dist'),
   },
   optimization: {
     // 启动标记功能
     usedExports: true,
+    // 启用 TerserPlugin, 或者命令接口使用 --optimize-minimize 标记
+    // minimize: true,
   }
 }
